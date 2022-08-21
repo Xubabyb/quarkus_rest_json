e17a055239edaa7f17c8b9583f3d377109c1d9c6
Dmitriy Shidlovskiy <xubabyb@gmail.com>
2022-08-21 22:53:02 +0300

    add comment

--- src/main/java/org/acme/rest/json/Legume.java
+++ src/main/java/org/acme/rest/json/Legume.java
@@ -3,6 +3,12 @@ package org.acme.rest.json;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
+/*
+TODO Эта аннотация обязательна, смотри внимательно,
+     после нативной компиляции при использовании Response JSON не сериализуется и получаем на проде пустую страницу,
+     вместо списка (смотри класс LegumeResource)
+     при запуске в dev работает и без неё, так как JVM работает с использованием рефлексии и прокси
+*/
 public class Legume {
 
     public String name;
